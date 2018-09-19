4d7db4b529382d2de9d7ff1ae2d522bf8e2be5b0
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-09-19 09:16:23 +0300

    condition tasks folder defined. IfTask1 defined.

---
+++ IfTask1/IfTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+
+
+        static void Main(string[] args)
+    {
+        Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen " +
+            "positiivinen, " +
+            "negatiivinen vai nolla.");
+        Console.WriteLine("syötä luku")
+
+
+        }
+}
+}
