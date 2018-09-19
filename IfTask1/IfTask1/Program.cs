baaed5c19a7ed219592842269e18b8afb576649f
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-09-19 09:59:30 +0300

    ifTask1 finished

--- IfTask1/IfTask1/Program.cs
+++ IfTask1/IfTask1/Program.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 
-namespace hello_world
+namespace ifTaks1
 {
     class Program
-
+    {
 
         static void Main(string[] args)
-    {
-        Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen " +
-            "positiivinen, " +
-            "negatiivinen vai nolla.");
-        Console.WriteLine("syötä luku")
+        {
+            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen " +
+                "positiivinen, " +
+                "negatiivinen vai nolla.");
+            Console.Write("syötä luku: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla!");
+            else if (number < 0)
+                Console.WriteLine($"Numero {number} on negatiivinen");
+            else
+                Console.WriteLine($"Numero {number} on positiivinen!");
 
 
         }
-}
+
+    }
 }
