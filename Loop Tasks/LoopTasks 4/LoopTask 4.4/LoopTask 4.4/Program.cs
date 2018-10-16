21393d835f098b1a94b0f70bb71c64c1abe06e03
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 16:49:40 +0300

    LoopTask 4.4 finished.

--- Loop Tasks/LoopTasks 4/LoopTask 4.4/LoopTask 4.4/Program.cs
+++ Loop Tasks/LoopTasks 4/LoopTask 4.4/LoopTask 4.4/Program.cs
@@ -6,7 +6,36 @@ namespace LoopTask_4._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma simuloi rahan heittoa seuraavasti:\nOhjelma kysyy ensin heittojen lukumäärää ja lopuksi tulostaa saatujen klaavojen ja kruunien määrän.");
+
+            Console.Write("Syötä heittojen lukumäärä: ");
+            int lkm = int.Parse(Console.ReadLine());
+
+            int rndNumber;
+            int klaava = 0;
+            int kruuna = 0;
+            int i = 0;
+
+            for (i = 0; i < lkm; i++)
+            {
+                Random rnd = new Random();
+                rndNumber = rnd.Next(0,2);
+
+                if (rndNumber == 1)
+                    {
+                    kruuna++;
+                }
+                else
+                {
+                    klaava++;
+                }
+
+            } Console.WriteLine("Heitit {0} kertaa", lkm);
+              Console.WriteLine("Kruuna tuli {0} kertaa", kruuna);
+              Console.WriteLine("Klaava tuli {0} kertaa", klaava);
+
+            Console.ReadKey();
+
         }
     }
 }
