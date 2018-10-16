2b6e7b0d010a59bebcfdf4fcbf73524cb2fd416a
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 11:34:24 +0300

    LoopTask 4.3 finished.

--- Loop Tasks/LoopTasks 4/LoopTask 4.3/LoopTask 4.3/Program.cs
+++ Loop Tasks/LoopTasks 4/LoopTask 4.3/LoopTask 4.3/Program.cs
@@ -7,6 +7,19 @@ namespace LoopTask_4._3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int rndNumber;
+            int i;
+
+            Random rnd = new Random();
+            rndNumber = rnd.Next(0,50);
+           
+            for (i = 1; i < 6; i++)
+            {
+                //rndNumber = rnd.Next(0, 50);
+                Console.WriteLine("Rivi " + i + ":" + rnd.Next(0, 50) + "," + rnd.Next(0, 50) + "," + rnd.Next(0, 50) +"," + rnd.Next(0, 50) +"," + rnd.Next(0, 50));
+            }
+            Console.ReadKey();
         }
     }
 }
