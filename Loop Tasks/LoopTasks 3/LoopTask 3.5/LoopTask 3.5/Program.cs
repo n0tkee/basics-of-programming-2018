f97d074fe934ec7fd8b0c56ebe51d85a7ce3337b
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 22:27:49 +0300

    LoopTask 3.5 finished.

--- Loop Tasks/LoopTasks 3/LoopTask 3.5/LoopTask 3.5/Program.cs
+++ Loop Tasks/LoopTasks 3/LoopTask 3.5/LoopTask 3.5/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
 
-namespace LoopTask_3._5
+namespace Looptask_3._2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee N:n ensimmäisen parittoman ja parillisen lukujen summan");
+
+            Console.Write("Syötä N! arvo: ");
+            int num = int.Parse(Console.ReadLine());
+            int f = 0;
+            int oddSum = 0;
+            int evenSum = 0;
+
+            if (num == 0)
+            {
+                Console.WriteLine("Määrittelemätön");
+            }
+
+            {
+                if (num > 0)
+                {
+                    for (int i = 1; i <= num; i++)
+                    {
+                        f = f + i;
+
+                        if (i % 2 == 0)
+                        {
+                            evenSum = evenSum + i;
+                        }
+                        else
+                        {
+                            oddSum = oddSum + i;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i >= num; i--)
+                    {
+                        f = (f) + (i);
+
+                        if (i % 2 == 0)
+                        {
+                            evenSum = (evenSum) + (i);
+                        }
+                        else
+                        {
+                            oddSum = (oddSum) + (i);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Parillisten summa on: {evenSum}");
+            Console.WriteLine($"Parittoimien summa on: {oddSum}");
+            Console.ReadKey();
         }
     }
 }
