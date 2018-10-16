c5ed5707fbb1495eacc9bbabadcb57f6eb79e3ed
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 17:16:18 +0300

    LoopTask 4.6 finished.

--- Loop Tasks/LoopTasks 4/LoopTask 4.6/LoopTask 4.6/Program.cs
+++ Loop Tasks/LoopTasks 4/LoopTask 4.6/LoopTask 4.6/Program.cs
@@ -6,7 +6,26 @@ namespace LoopTask_4._6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma simuloi nopanheittoa 1000 kertaa\nja tulostaa lisäksi kuutosen esiintymiskertojen lukumäärän:");
+
+            int rndNumber;
+            int kutonen = 0;
+
+            Random rnd = new Random();
+            rndNumber = rnd.Next(0, 6);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                rndNumber = rnd.Next(1, 7);
+
+                if (rndNumber == 6)
+                {
+                    kutonen++;
+                }
+                Console.WriteLine($"{i + 1}.\t{rndNumber}");
+            }
+            Console.WriteLine("Kuutonen esiintyi {0} kertaa", kutonen);
+            Console.ReadKey();
         }
     }
 }
