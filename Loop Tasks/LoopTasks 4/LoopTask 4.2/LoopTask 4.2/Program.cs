06b126c4c252486ea336d115ee57cfd81b953db0
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 10:55:13 +0300

    LoopTask 4.2 finished.

--- Loop Tasks/LoopTasks 4/LoopTask 4.2/LoopTask 4.2/Program.cs
+++ Loop Tasks/LoopTasks 4/LoopTask 4.2/LoopTask 4.2/Program.cs
@@ -6,7 +6,31 @@ namespace LoopTask_4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1 - 9");
+
+            int i;
+
+            Console.Write("Syötä Luku: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 1) 
+            {
+                Console.WriteLine("Määrittelemätön");
+            }
+            else if (num>9)
+            {
+                Console.WriteLine("Määrittelemätön");
+
+            }
+            else
+            {
+                for (i =1; i <= 9; i++)
+                {
+                    Console.WriteLine(num + "*" + i + "=" + num * i);
+                }
+            }
+            Console.ReadLine();
+
         }
     }
 }
