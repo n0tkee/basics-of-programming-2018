41989555e8f1404bb8fac57077ce682cae76f1a8
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 16:49:19 +0300

    LoopTask 4.3 fixed.

--- Loop Tasks/LoopTasks 4/LoopTask 4.3/LoopTask 4.3/Program.cs
+++ Loop Tasks/LoopTasks 4/LoopTask 4.3/LoopTask 4.3/Program.cs
@@ -6,18 +6,17 @@ namespace LoopTask_4._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa 20 satunnaista kokonaislukua väliltä [0-50].\n");
 
             int rndNumber;
             int i;
 
             Random rnd = new Random();
-            rndNumber = rnd.Next(0,50);
+            rndNumber = rnd.Next();
            
             for (i = 1; i < 6; i++)
             {
-                //rndNumber = rnd.Next(0, 50);
-                Console.WriteLine("Rivi " + i + ":" + rnd.Next(0, 50) + "," + rnd.Next(0, 50) + "," + rnd.Next(0, 50) +"," + rnd.Next(0, 50) +"," + rnd.Next(0, 50));
+                Console.WriteLine("Rivi " + i + ": " + rnd.Next(0, 50) + "," + rnd.Next(0, 50) + "," + rnd.Next(0, 50) +"," + rnd.Next(0, 50) +"," + rnd.Next(0, 50));
             }
             Console.ReadKey();
         }
