41420b9c476bd651d16f65c22d42aa154209a4b4
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-16 10:29:38 +0300

    LoopTasks 4 folder initialized.

---
+++ Loop Tasks/LoopTasks 4/LoopTask 4.1/LoopTask 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ifTask4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rndNumber;
+
+            rndNumber = rnd.Next();
+            //rnd.NextDouble();
+
+            Console.WriteLine("Ohjelma tulostaa 10 arvottua kokonaislukua ja niiden neliöjuuret");
+            Console.WriteLine("#\tRND\tSQRT");
+            for (int i = 0; i < 10; i++)
+            {
+                rndNumber = rnd.Next(0, 100);
+                Console.WriteLine($"{i + 1}.\t{rndNumber}\t{Math.Sqrt(rndNumber)}");
+
+            }
+        }
+    }
+}
