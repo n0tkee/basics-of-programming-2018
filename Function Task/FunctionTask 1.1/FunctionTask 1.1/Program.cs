2b3332a7ef6b651d6e9ec02dae5e0d109e9c4f5e
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-17 13:35:07 +0300

    Function Task 1 initialized and finished.

---
+++ Function Task/FunctionTask 1.1/FunctionTask 1.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FunctionTask_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa annetun määrän tähtiä!");
+
+            int n;
+            while (true)
+            {
+                Console.Write("Tähtien lukumäärä: \n");
+                n = int.Parse(Console.ReadLine());
+
+                if (n < 0)
+                {
+                    Console.WriteLine("Luku {0} ei ole sallittu luku", n);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            string asterisks = getStars(n);
+            Console.WriteLine(asterisks);
+            Console.ReadKey();
+
+        }
+        static string getStars(int n)
+        {
+            string res = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                res += '*';
+            }
+            return res;
+        }
+    }
+}
