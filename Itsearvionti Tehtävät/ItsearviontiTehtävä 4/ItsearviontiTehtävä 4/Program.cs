cec50aa813dbd123b3de6534d3e246ea621998f5
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-24 15:10:49 +0300

    ItsearviontiTehtävä 4. initialized.

---
+++ Itsearvionti Tehtävät/ItsearviontiTehtävä 4/ItsearviontiTehtävä 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ItsearviontiTehtävä_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
