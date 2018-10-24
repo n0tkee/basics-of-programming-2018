1aa26ebd86dcd5688c9e4c6c68ee97bb0c6a1445
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-24 12:57:41 +0300

    ItsearviontiTehtävä 2. initialized.

--- Itsearvionti Tehtävät/ItsearviontiTehtävä 2/ItsearviontiTehtävä 2/Program.cs
+++ Itsearvionti Tehtävät/ItsearviontiTehtävä 2/ItsearviontiTehtävä 2/Program.cs
@@ -6,21 +6,7 @@ namespace ItsearviontiTehtävä_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma tulostaa käyttäjän syöttämän lauseen tai sanan niin monta kertaa kuin sanassa tai lauseessa on kirjaimia.\n");
-
-            Console.Write("Syötä sana tai lause: ");
-            string userInput = Console.ReadLine();
-
-            int i = 0;
-
-            for (i = 0; i < userInput.Length; i++)
-            {
-                Console.WriteLine(userInput);
-
-            }
-
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
         }
     }
 }
-
