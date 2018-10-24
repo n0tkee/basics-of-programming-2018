27dc14aacd87fe1ff8eb90db8ca98b0e84a6a742
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-24 12:25:22 +0300

    Itsearvionti Tehtävä 1.1 initialized.

---
+++ Itsearvionti Tehtävät/ItsearviontiTehtävä 1.1/ItsearviontiTehtävä 1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ItsearviontiTehtävä_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Syötä sana tai lause: ");
+            string userInput = Console.ReadLine();
+
+            int i = 0;
+
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(userInput);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
