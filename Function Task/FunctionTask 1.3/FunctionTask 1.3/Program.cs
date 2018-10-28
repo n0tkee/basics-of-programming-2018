1e21b3949b4e9aa6cf741858c81e41eb704933af
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-28 19:05:37 +0200

    FunctionTask 1.3 finished.

--- Function Task/FunctionTask 1.3/FunctionTask 1.3/Program.cs
+++ Function Task/FunctionTask 1.3/FunctionTask 1.3/Program.cs
@@ -6,7 +6,39 @@ namespace FunctionTask_1._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string retNumber = numberFromRange(6, 70);
+            Console.WriteLine(retNumber);
+
+            Console.ReadKey();
         }
+
+        public static string numberFromRange(int lowerBound, int upperBound)
+        {
+            int num;
+       
+            do
+            {
+                Console.Write("Syötä luku väliltä 6-70: ");
+                num = int.Parse(Console.ReadLine());
+
+                if (num < lowerBound)
+                {
+                    Console.WriteLine("Luku on liian pieni!\n");
+                }
+                else if (num > upperBound)
+                {
+                    Console.WriteLine("Luku on liian suuri!\n");
+                }
+                else
+                {
+                    return "";
+                }
+
+            }
+
+            while (num > lowerBound || num < upperBound) ;
+            return "Syötä uusi luku:";
+        }
+        
     }
 }
