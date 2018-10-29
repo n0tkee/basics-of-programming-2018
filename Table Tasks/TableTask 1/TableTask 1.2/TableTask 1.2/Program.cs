1a46305a817c32159ed704af759e1fe8adb46919
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-29 14:18:26 +0200

    TableTask 1.2 initialized.

---
+++ Table Tasks/TableTask 1/TableTask 1.2/TableTask 1.2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableTask_1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
