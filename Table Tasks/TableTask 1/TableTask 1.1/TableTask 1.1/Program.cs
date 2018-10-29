0afb8056b1825304f41ffa5580b2bd1904d9245c
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-10-29 14:18:03 +0200

    TableTask 1.1. finished.

--- Table Tasks/TableTask 1/TableTask 1.1/TableTask 1.1/Program.cs
+++ Table Tasks/TableTask 1/TableTask 1.1/TableTask 1.1/Program.cs
@@ -6,7 +6,26 @@ namespace TableTask_1._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            decimal[] productPrice = { .96m , 72.0m, 99.90m};
+            int[] productAmount = { 1, 3, 2};
+            decimal[] productTotalPrice = new decimal[3];
+            productTotalPrice[0] = productPrice[0] * productAmount[0];
+            productTotalPrice[1] = productPrice[1] * productAmount[1];
+            productTotalPrice[2] = productPrice[2] * productAmount[2];
+
+            //Vaihtoehto #1
+            //Console.WriteLine("Tuote 1: " + productTotalPrice[0]);
+            //Console.WriteLine("Tuote 2: " + productTotalPrice[1]);
+            //Console.WriteLine("Tuote 3: " + productTotalPrice[2]);
+
+            //Vaihtoehto #2
+            for (int i = 0; i < productTotalPrice.Length; i++)
+            {
+                Console.WriteLine($"Tuote {i + 1}: " + productTotalPrice[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
