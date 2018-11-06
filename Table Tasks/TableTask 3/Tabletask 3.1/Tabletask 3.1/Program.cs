e9ab7027d6d32c5ade0bf1f869c0e4b0951c99fa
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-11-06 13:05:48 +0200

    TableTask 3.1 finished

---
+++ Table Tasks/TableTask 3/Tabletask 3.1/Tabletask 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabletask_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rnd = new Random();
+            int oridinalNum = 0;
+
+            Console.WriteLine("[X] = Arvo");
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0,20);
+                oridinalNum++;
+                Console.WriteLine("[{0}] = {1}", oridinalNum, array[i].ToString("D2"));
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
