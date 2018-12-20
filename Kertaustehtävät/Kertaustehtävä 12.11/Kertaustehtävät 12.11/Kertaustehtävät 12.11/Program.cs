31206ecc549a8fdf90369bb199e6751dd9f75ba5
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-12-20 13:03:34 +0200

    Kertaustehtävät 12.11 initialized.

---
+++ Kertaustehtävät/Kertaustehtävä 12.11/Kertaustehtävät 12.11/Kertaustehtävät 12.11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kertaustehtävät_12._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
